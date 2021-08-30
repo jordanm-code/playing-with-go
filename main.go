35c67417be847cff604127f49838d481f53ca15e
Jordan Code <jordan@jcprogramming.com>
2021-08-30 07:05:57 -0400

    Base our docker image off of ubuntu-lts

--- main.go
+++ main.go
@@ -3,6 +3,6 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("Test file for docker. \n")
-  fmt.Println("If this works, yay. I have something as a base for now. \n")
+  fmt.Println("Test file for docker.")
+  fmt.Println("If this works, yay. I have something as a base for now.")
 }
