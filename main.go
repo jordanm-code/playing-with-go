c9f9d480033522fb169818a181d11f54ce8c5f22
Jordan Code <jordan@jcprogramming.com>
2021-08-30 08:56:00 -0400

    Adding HTTP, and Printing our the results
    Fix the response

--- main.go
+++ main.go
@@ -1,8 +1,35 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "log"
+  "net/http"
+  "encoding/json"
+)
 
 func main() {
-  fmt.Println("Test file for docker.")
-  fmt.Println("If this works, yay. I have something as a base for now.")
+  fmt.Println("Starting up HTTP...")
+
+  xchgURL := "https://api.exchangerate.host/latest"
+
+  client := &http.Client{}
+
+  resp, err := client.Get(xchgURL)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  if resp.StatusCode == http.StatusOK {
+    var result map[string]interface{}
+
+    fmt.Println("Response:")
+    fmt.Println(resp)
+
+    fmt.Println("Body")
+    fmt.Println(resp.Header)
+    fmt.Println(resp.Body)
+
+    json.NewDecoder(resp.Body).Decode(&result)
+    fmt.Println(result["rates"])
+  }
 }
